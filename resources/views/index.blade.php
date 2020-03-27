087729c44c75ba95bac30f0c3b11c936ec07df7a
YasirAlawa <yasir.alawa@tatweer.sy>
2020-03-27 14:34:05 +0300

    fix title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,20 +4,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="description" content="{{ $article ? $article->title : 'SYRIAN RESEARCHERS' }}"/>
-    <meta property="og:description" content="{{ $article ? $article->title : 'SYRIAN RESEARCHERS'    }}"/>
-    <meta property="og:url" content="{{ $article ? 'https://svu-res.me/article/'. $article->id : 'https://svu-res.me'    }}"/>
-    <meta property="og:title" content="{{ 'SYRIAN RESEARCHERS' }}"/>
-    <meta property="og:image"     content="{{ $article ? asset('image/'.$article->homePicture->path) : ''}}"/>
-    <meta property="og:image:url" content="{{ $article ? asset('image/'.$article->homePicture->path) : ''    }}"/>
-    <meta property="og:image:secure_url" content="{{ $article ? asset('image/'.$article->homePicture->path) : ''    }}"/>
+    <meta name="description" content="{{ $article ? $article->title : 'GIF Browsing' }}"/>
     <meta property="og:image:width"  content="1240"/>
     <meta property="og:image:height" content="650"/>
-    <meta property="og:type" content="article">
+
     <meta property="og:locale" content="ar_AR">
-    <link rel="canonical" href="{{ $article ? 'https://svu-res.me/article/'. $article->id : 'https://svu-res.me'    }}">
+    
     <meta property="fb:pages" content="111362890429466" />
-    <title>SYR-RES</title>
+    <title>GIF Browsing</title>
 
     <!-- Fonts -->
     <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
@@ -32,7 +26,7 @@
 <script src="{{ asset('js/app.js') }}" defer></script>
 <script>
     window.Laravel = {
-        "baseUrl": "https:\/\/svu-res.me\/"
+        "baseUrl": "http://localhost:8000/"
     }
 </script>
 </body>
